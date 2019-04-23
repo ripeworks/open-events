96ca3d3f791a63228f21cc62c7f8be2b86c12b80
Mike Kruk <tamagokun@gmail.com>
2019-04-23 19:41:34 -0400

    making some progress on the FE

---
+++ web/pages/_app.js
@@ -0,0 +1,31 @@
+import App, { Container } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../theme";
+
+export default class extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Head>
+          <title>Northport Omena Calendar</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
--- web/pages/new.js
+++ web/pages/new.js
@@ -1,1 +1,28 @@
-// Submit an event
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+
+export default class Page extends React.Component {
+  onSubmit = (e) => {
+      e.preventDefault();
+      // submit to API
+  }
+
+  render() {
+    return (
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h1">Northport Omena Calendar</Typography>
+          <form>
+            <TextField
+              defaultValue="Bare"
+              margin="normal"
+              variant="filled"
+            />
+          </form>
+        </Box>
+      </Container>
+    );
+  }
+}
