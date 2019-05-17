05be8ba939a23d8abb075c299b54bfa39217b235
Mike Kruk <mike@ripeworks.com>
2019-05-17 14:40:34 -0400

    set up landing page so it's not blank

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,9 +1,21 @@
 // @flow
+import "antd/dist/antd.css";
+import "../styles/app.css";
+import { Button } from "antd";
+import Link from "next/link";
+import Header from "../components/Header";
 
 const Index = () => (
-  <div>
-    {/* render calendar */}
-  </div>
+  <main>
+    <Header />
+    <section>
+      <center style={{ padding: 40 }}>
+        <Link href="/new">
+          <Button size="large">Submit Event</Button>
+        </Link>
+      </center>
+    </section>
+  </main>
 );
 
 export default Index;
