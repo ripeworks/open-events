0dbce8f510ca0e4d706a7cab4539afe3c200678c
Mike Kruk <mike@ripeworks.com>
2019-05-21 20:55:07 -0400

    lots of awesome work

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,21 @@
 // @flow
 import "antd/dist/antd.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import "../styles/app.css";
+import fetch from "isomorphic-fetch";
 import { Button } from "antd";
 import Link from "next/link";
-import Header from "../components/Header";
+import Router from "next/router";
+import BigCalendar from "react-big-calendar";
+import moment from "moment";
+import Modal from "react-responsive-modal";
 
-const Index = () => (
+import Header from "../components/Header";
+import EventDetail from "../components/EventDetail";
+
+const localizer = BigCalendar.momentLocalizer(moment);
+
+const Index = ({ events, id }) => (
   <main>
     <Header />
     <section>
@@ -14,8 +24,29 @@ const Index = () => (
           <Button size="large">Submit Event</Button>
         </Link>
       </center>
+      <BigCalendar
+        localizer={localizer}
+        events={events}
+        titleAccessor="summary"
+        startAccessor={event => event.start.dateTime}
+        endAccessor={event => event.end.dateTime}
+        onSelectEvent={event => {
+          Router.push(`/?id=${event.id}`, `/event/${event.id}`);
+        }}
+      />
+      <Modal open={!!id} onClose={() => Router.push("/")}>
+        {id && <EventDetail event={events.find(event => event.id === id)} />}
+      </Modal>
     </section>
   </main>
 );
+
+Index.getInitialProps = async ctx => {
+  const res = await fetch(`http://localhost:3000/api/list`);
+  const { etag, syncToken, items } = await res.json();
+  const { id } = ctx.query;
+
+  return { events: items, id };
+};
 
 export default Index;
