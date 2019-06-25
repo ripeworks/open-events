fda7e4491982142ca97dd677422d57aff65e5410
Mike Kruk <mike@ripeworks.com>
2019-06-25 11:20:32 -0400

    added intro copy

--- web/components/Header.js
+++ web/components/Header.js
@@ -1,13 +1,36 @@
 import Head from "next/head";
 import "../styles/header.css";
 
-export default () => (
+export default ({ intro = false }) => (
   <header>
-  <Head>
-    <title>Northport Omena Calendar</title>
-  </Head>
+    <Head>
+      <title>Northport Omena Calendar</title>
+    </Head>
     <hgroup>
       <h1>Northport Omena Calendar</h1>
+      {intro && (
+        <>
+          <p>
+            Come discover Northport and Omena events, places to visit, and
+            things to do in Leelanau Township.
+          </p>
+          <details>
+            <summary>
+              <a>Learn More</a>
+            </summary>
+            <p>
+              The event calendar is a one-stop place to connect you -- our
+              community of longtime residents, newcomers and visitors -- with
+              all our local events. Find meeting times for Leelanau Township and
+              Northport Village. Get connected with local civic organization and
+              volunteer opportunities in Northport and Omena. It's an all-in-one
+              guide to fun and community connections in these two historic Lake
+              Michigan towns at the north end of Leelanau Peninsula.
+            </p>
+            <p>Click on a calendar item for additional information.</p>
+          </details>
+        </>
+      )}
     </hgroup>
   </header>
 );
--- web/pages/index.js
+++ web/pages/index.js
@@ -37,7 +37,7 @@ const Index = ({ events, id }) => {
 
   return (
     <main>
-      <Header />
+      <Header intro />
       <section>
         <div className="toolbar">
           {view === "calendar" && (
