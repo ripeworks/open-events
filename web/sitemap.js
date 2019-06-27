7360e2c8d8a024d2bbbd9af63a8898c2951992aa
Mike Kruk <mike@ripeworks.com>
2019-06-27 08:41:51 -0400

    added a sitemap!

---
+++ web/sitemap.js
@@ -0,0 +1,34 @@
+const builder = require("xmlbuilder");
+const fetch = require("node-fetch");
+
+const getUrl = (path = "/") => {
+  return `https://northportomenacalendar.com${path}`;
+};
+
+const getSitemap = async () => {
+  const map = {
+    urlset: {
+      "@xmlns": "http://www.sitemaps.org/schemas/sitemap/0.9",
+      "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+      "@xsi:schemaLocation":
+        "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd",
+      url: [{ loc: getUrl() }]
+    }
+  };
+
+  const eventsRes = await fetch(getUrl("/api/list"));
+  const events = await eventsRes.json();
+
+  events.items.map(event => {
+    map.urlset.url.push({ loc: getUrl(`/event/${event.id}`) });
+  });
+
+  const sitemap = builder.create(map, { encoding: "utf-8" });
+  return sitemap.end({ pretty: true });
+};
+
+module.exports = async (req, res) => {
+  const sitemap = await getSitemap();
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(sitemap);
+};
