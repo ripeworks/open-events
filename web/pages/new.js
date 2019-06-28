46f1c88f5653aa46ba3a48bc842deeb7976bd9d3
Mike Kruk <mike@ripeworks.com>
2019-06-28 09:32:30 -0400

    added a back to calendar button

--- web/pages/new.js
+++ web/pages/new.js
@@ -1,7 +1,8 @@
 import "antd/dist/antd.css";
 import "../styles/app.css";
 import fetch from "unfetch";
-import { Alert, message } from "antd";
+import { Alert, Button, Icon, message } from "antd";
+import Link from "next/link";
 import Header from "../components/Header";
 import EventForm from "../components/EventForm";
 
@@ -37,8 +38,23 @@ export default class Page extends React.Component {
       <main>
         <Header />
         <section>
-          <p>Please fill out the form below to submit an event to the Northport Omena Calendar. Events require approval before they will be visible on the calendar, so please allow up to 72 hours for approval.</p>
-          <p>All events must meet the following criteria to be eligible for posting:</p>
+          <p>
+            <Link href="/">
+              <Button size="large">
+                <Icon type="left" />
+                Back to Calendar
+              </Button>
+            </Link>
+          </p>
+          <p>
+            Please fill out the form below to submit an event to the Northport
+            Omena Calendar. Events require approval before they will be visible
+            on the calendar, so please allow up to 72 hours for approval.
+          </p>
+          <p>
+            All events must meet the following criteria to be eligible for
+            posting:
+          </p>
           <ul>
             <li>Available to public (no membership only or private events)</li>
             <li>Take place in Leelanau Township</li>
