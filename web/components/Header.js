28647f2efb72bd30e8c7b7f30c3977fa57598180
Mike Kruk <mike@ripeworks.com>
2019-07-02 13:05:06 -0400

    added meta description

--- web/components/Header.js
+++ web/components/Header.js
@@ -5,6 +5,17 @@ export default ({ intro = false }) => (
   <header>
     <Head>
       <title>Northport Omena Calendar</title>
+      <meta
+        name="description"
+        content="Come discover Northport and Omena events, places to visit, and
+            things to do in Leelanau Township. The event calendar is a one-stop place to connect you -- our
+            community of longtime residents, newcomers and visitors -- with
+            all our local events. Find meeting times for Leelanau Township and
+            Northport Village. Get connected with local civic organization and
+            volunteer opportunities in Northport and Omena. It's an all-in-one
+            guide to fun and community connections in these two historic Lake
+            Michigan towns at the north end of Leelanau Peninsula."
+      />
     </Head>
     <hgroup>
       <h1>Northport Omena Calendar</h1>
