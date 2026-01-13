953b39cc95eebba022837366a2453b86db3c78b6
Mike Kruk <mike@ripeworks.com>
2026-01-12 20:00:20 -0500

    move to env var

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,12 @@ class MyDocument extends Document {
       <Html lang="en" dir="ltr">
         <Head>
           <meta charSet="utf-8" />
-          <script
-            type="text/javascript"
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZ39ZTYG_sdJdzaIBPBb_oMeKsIGs_xPc&libraries=places"
-          />
+          {process.env.MAPS_API_KEY && (
+            <script
+              type="text/javascript"
+              src={`https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API_KEY}&libraries=places`}
+            />
+          )}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
